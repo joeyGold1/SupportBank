c9c65406465f06bf2a16268b19b658a3d4e5d8fc
Joey Gold <joey.gold@softwire.com>
2021-07-07 10:55:18 +0100

    Finished part 1 of task - List and List All work for well formatted CSV files.

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 import * as luxon from 'luxon';
 import { toNamespacedPath } from 'path/posix';
-
+const readline = require('readline');
 const csv = require('csv-parse');
 const fs = require('fs');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 
 class Person {
     name:string;
@@ -10,6 +16,9 @@ class Person {
     balance = 0;
     constructor(name:string){
         this.name = name;
+    }
+    stringDetails(padding:number){
+        return (this.name.padEnd(padding))+(((this.balance/100).toString()).padStart(7))
     }
 }
 
@@ -27,9 +36,11 @@ class Transaction {
         this.date = date;
         this.narrative = narrative;
     }
+    stringDetails(padding:number){
+        return this.date.toLocaleString().padEnd(padding)+this.to.name.padEnd(padding)+this.from.name.padEnd(padding)+this.narrative.padEnd(padding*2)+(this.amount/100).toString().padStart(6)
+    }
     
 }
-
 
 
 
@@ -51,9 +62,49 @@ function processCSV(filename:string){
 
 //Main processing of program happens here.
 function mainProcess(rows:string[][]) {
-     var people:Map<string,Person> = createPeople(rows);
-     var transactions:Transaction[] = createTransactions(people,rows);
+     let people:Map<string,Person> = createPeople(rows);
+     let transactions:Transaction[] = createTransactions(people,rows);
+     userInputLoop(people,transactions);
  }
+
+
+ ////NEEDS FINISHING
+function userInputLoop(people: Map<string,Person>,transactions: Transaction[]){
+    rl.question('Enter command... ', (userInput:string) => {
+        if (userInput == "List All"){
+            let peopleArray = Array.from(people.values());
+            console.log("Name                  Balance")
+            for (let i=0;i<peopleArray.length;i++) {
+                console.log(peopleArray[i].stringDetails(22));
+            }
+        }
+        else if (userInput.startsWith("List ")){
+            let name:string = userInput.slice(5);
+            let person = people.get(name);
+            if (person == undefined) {
+                console.log("Name "+name+" is not in file. ");
+            }
+            else {
+                console.log("Date              To                From              "+"Narrative".padEnd(36)+"Amount");
+                for (let i = 0;i<person.transactions.length;i++){
+                    console.log(person.transactions[i].stringDetails(18));
+                }
+            }
+        }
+        else{
+            console.log("Invalid command, please enter 'List All' or 'List <name>'. ");
+        }
+
+
+
+        rl.close();
+      });
+}
+    
+function outputTransactionDetails(transaction:Transaction){}
+
+
+
 
 //Creates a map (dictionary) of each person, indexed by their name.
 function createPeople(rows:string[][]){
@@ -75,16 +126,18 @@ function createPeople(rows:string[][]){
 //Creates a list of every transaction and stores a record of it with each relevant person's data.
 function createTransactions(people:Map<string,Person>,rows:string[][]){
     let transactions: Transaction[] = [];
+    let fromPerson;
+    let toPerson;
     for (let i = 1; i<rows.length;i++){
         let date = luxon.DateTime.fromFormat(rows[i][0], "dd/MM/yyyy");
-        let fromPerson = people.get(rows[i][1]);
-        let toPerson = people.get(rows[i][2]);
+        fromPerson = people.get(rows[i][1]);
+        toPerson = people.get(rows[i][2]);
         //Compiler thinks these may be undefined. I don't think that is possible but this makes it happy.
         if (fromPerson== undefined || toPerson==undefined){
             console.log("Error: Person on traction " + i+ " does not exist")
         }
         else{
-            let amount = Number(rows[i][4]);
+            let amount = Number(rows[i][4])*100;
             let currentTransaction = new Transaction(date,fromPerson,toPerson,rows[i][3],amount);
             transactions.push(currentTransaction);
             fromPerson.balance -= amount;
